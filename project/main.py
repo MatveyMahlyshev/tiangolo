961eb959fcc77485a42789441cedd2c05807dbcf
MatveyMahlyshev <matthewfootballer11@gmail.com>
2024-11-07 16:26:36 +0300

    query params

--- project/main.py
+++ project/main.py
@@ -6,12 +6,22 @@ class ModelName(str, Enum):
     resnet = "resnet"
     lenet = "lenet"
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 app = FastAPI()
 
+@app.get("/items/")
+async def get_items(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
+
 @app.get("/items/{item_id}")
-async def root(item_id: int):
-    return {"item_id": item_id}
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "description"})
+    return item
 
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
