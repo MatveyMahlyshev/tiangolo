c244ef33c6666ff5c5bc5392dc1dbe948910a247
MatveyMahlyshev <matthewfootballer11@gmail.com>
2024-11-07 16:39:30 +0300

    body params

--- project/main.py
+++ project/main.py
@@ -1,33 +1,17 @@
 from fastapi import FastAPI
-from enum import Enum
+from typing import Union
 
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
+from pydantic import BaseModel
 
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
 
 app = FastAPI()
 
-@app.get("/items/")
-async def get_items(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip: skip + limit]
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: str, q: str | None = None, short: bool = False):
-    item = {"item_id": item_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update({"description": "description"})
+@app.post("/items/")
+async def create_item(item: Item):
     return item
 
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep learn"}
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-    return {"model_name": model_name, "message": "smth"}
-    
